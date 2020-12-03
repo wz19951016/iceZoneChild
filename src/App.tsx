7e2fd3d6c1e397f2233ae0a6cecf01a57d3e004d
WangZhong <619324237@qq.com>
2020-12-03 17:31:41 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import {{{name}}InitAction} from "./actions"
+import Provider from "./provider";
+import connect from "./connect";
+
+const historySelf = createHistory();
+
+
+const App = (props) => {
+  const { dispatch } = props;
+
+  useEffect(() => {
+    console.log(props);
+    dispatch({{name}}InitAction())
+  }, [0]);
+
+  return (
+  <div>{{name}}</div>
+  );
+};
+const BaseApp = connect((state) => {
+  return { ...state };
+})(App);
+
+const WrapApp = ({ globalEventDistributor, history }) => {
+  console.log(globalEventDistributor);
+  return (
+    <Provider store={globalEventDistributor}>
+      <BaseApp history={history} />
+    </Provider>
+  );
+};
+
+export default WrapApp;
